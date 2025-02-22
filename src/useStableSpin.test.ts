c9189766ae33d708093bca7a8ff305577924ed61
Nils Haberkamp <haberkamp.n@gmail.com>
2025-02-22 19:13:01 +0100

    chore: do not show spinner before network request finishes

--- src/useStableSpin.test.ts
+++ src/useStableSpin.test.ts
@@ -1,5 +1,32 @@
-import { test, expect } from "vitest";
+import { test, expect, beforeAll, vi, afterAll } from "vitest";
+import { act, renderHook } from "@testing-library/react";
+import { useStableSpin } from "./useStableSpin.js";
 
-test("add", () => {
-  expect(1 + 1).toBe(2);
+beforeAll(() => {
+  vi.useFakeTimers();
+});
+
+afterAll(() => {
+  vi.runOnlyPendingTimers();
+  vi.useRealTimers();
+});
+
+test("does not show the spinner when the request finishes before the delay", () => {
+  // ARRAGE
+  const hook = renderHook(useStableSpin, {
+    initialProps: false,
+  });
+
+  // ACT
+  // Start network request
+  hook.rerender(true);
+
+  // Finish network request
+  act(() => {
+    vi.advanceTimersByTime(50);
+    hook.rerender(true);
+  });
+
+  // ASSERT
+  expect(hook.result.current).toBe(false);
 });
--- src/useStableSpin.ts
+++ src/useStableSpin.ts
@@ -1,1 +1,7 @@
-export function useStableSpin() {}
+import { useState } from "react";
+
+export function useStableSpin(isLoading: boolean) {
+  const [showSpinner] = useState(false);
+
+  return showSpinner;
+}

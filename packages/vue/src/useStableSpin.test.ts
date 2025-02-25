20ad9afc6cbaa8951c5f5b77261360d75d4673ab
Nils Haberkamp <haberkamp.n@gmail.com>
2025-02-25 17:17:46 +0100

    feat(vue): add useStableSpin composable

--- packages/vue/src/useStableSpin.test.ts
+++ packages/vue/src/useStableSpin.test.ts
@@ -1,5 +1,189 @@
-import { test, expect } from "vitest";
+import { test, expect, vi, afterEach, beforeEach } from "vitest";
+import flushPromises from "flush-promises";
+import { useStableSpin } from "./useStableSpin.js";
+import { ref } from "vue";
 
-test("", () => {
-  expect(1).toBe(1);
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+afterEach(() => {
+  vi.runOnlyPendingTimers();
+  vi.useRealTimers();
+});
+
+test("does not show the spinner when the request finishes before the delay", async () => {
+  // ARRANGE
+  const options = {
+    delay: 100,
+    minDuration: 100,
+  };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // ACT
+  // Start network request
+  isLoading.value = true;
+  await flushPromises();
+
+  // Finish network request
+  vi.advanceTimersByTime(50);
+  await flushPromises();
+
+  isLoading.value = false;
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(false);
+});
+
+test("shows the spinner when the request takes longer than the delay", async () => {
+  // ARRANGE
+  const options = {
+    delay: 100,
+    minDuration: 100,
+  };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // ACT
+  // Start the network request
+  isLoading.value = true;
+  await flushPromises();
+
+  vi.advanceTimersByTime(100);
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(true);
+});
+
+test("shows the spinner when the request finishes before the minimum duration", async () => {
+  // ACT
+  const options = {
+    delay: 100,
+    minDuration: 100,
+  };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // Start the network request
+  isLoading.value = true;
+  await flushPromises();
+
+  // Finish the network request
+  vi.advanceTimersByTime(150);
+  await flushPromises();
+
+  isLoading.value = false;
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(true);
+});
+
+test("does not show a spinner when the request finishes after the minimum duration", async () => {
+  // ARRANGE
+  const options = {
+    delay: 100,
+    minDuration: 100,
+  };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // ACT
+  // Start the network request
+  isLoading.value = true;
+  await flushPromises();
+
+  // Finish the network request
+  vi.advanceTimersByTime(200);
+  await flushPromises();
+
+  isLoading.value = false;
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(false);
+});
+
+test("shows a spinner when the request takes longer than the minimum duration", async () => {
+  // ARRANGE
+  const options = {
+    delay: 100,
+    minDuration: 100,
+  };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // ACT
+  // Start the network request
+  isLoading.value = true;
+  await flushPromises();
+
+  vi.advanceTimersByTime(250);
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(true);
+});
+
+test("it is possible to customise the delay", async () => {
+  // ARRANGE
+  const options = { delay: 200 };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // ACT
+  // Start network request
+  isLoading.value = true;
+  await flushPromises();
+
+  vi.advanceTimersByTime(150);
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(false);
+
+  // ACT
+  vi.advanceTimersByTime(50);
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(true);
+});
+
+test("it is possible to customise the minimum duration", async () => {
+  // ARRANGE
+  const options = { minDuration: 200 };
+
+  const isLoading = ref(false);
+  const hook = useStableSpin(isLoading, options);
+
+  // ACT
+  // Start network request
+  isLoading.value = true;
+  await flushPromises();
+
+  vi.advanceTimersByTime(250);
+  await flushPromises();
+
+  isLoading.value = false;
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(true);
+
+  // ACT
+  vi.advanceTimersByTime(50);
+  await flushPromises();
+
+  // ASSERT
+  expect(hook.value).toBe(false);
 });
--- packages/vue/src/useStableSpin.ts
+++ packages/vue/src/useStableSpin.ts
@@ -1,1 +1,44 @@
-export function useStableSpin() {}
+import { computed, ref, watch, type Ref } from "vue";
+
+export const DEFAULT_DELAY = 100;
+export const DEFAULT_MIN_DURATION = 100;
+
+const DEFAULT_OPTIONS = {
+  delay: DEFAULT_DELAY,
+  minDuration: DEFAULT_MIN_DURATION,
+};
+
+export function useStableSpin(
+  isLoading: Ref<boolean>,
+  options?: Partial<typeof DEFAULT_OPTIONS>
+) {
+  const config = Object.assign({}, DEFAULT_OPTIONS, options);
+
+  const state = ref<"idle" | "delay" | "show" | "expired">("idle");
+  const timeout = ref<ReturnType<typeof setTimeout> | null>(null);
+
+  watch([isLoading, state], () => {
+    if (isLoading.value && state.value === "idle") {
+      if (timeout.value) clearTimeout(timeout.value);
+      state.value = "delay";
+
+      timeout.value = setTimeout(() => {
+        state.value = "show";
+
+        timeout.value = setTimeout(() => {
+          state.value = "expired";
+        }, config.minDuration);
+      }, config.delay);
+    }
+
+    if (!isLoading.value && state.value !== "show") {
+      state.value = "idle";
+    }
+  });
+
+  const showSpinner = computed(() => ["show", "expired"].includes(state.value));
+
+  // TODO: clear timout when unmounting the component
+
+  return showSpinner;
+}
